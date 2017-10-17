7268ace7b364a1cb31197033b363e854d03826f9
sneha <kandpalsneha@gmail.com>
2017-10-18 03:16:50 +0530

    Simplified predict.m

--- Logistic Regression/ex2/predict.m
+++ Logistic Regression/ex2/predict.m
@@ -15,12 +15,8 @@ p = zeros(m, 1);
 %               You should set p to a vector of 0's and 1's
 %
 
-h = sigmoid(X * theta);
 
-p = h > 0.5;
-
-
-
+p = X * theta > 0;
 
 
 % =========================================================================
